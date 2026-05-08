70cc393757e373e577f35a16e68133fb9766d321
Anurima2206 <anutri2k05@gmail.com>
2026-05-08 20:08:27 +0530

    bg color changer added

--- Projects_react/Colour_changer/src/App.jsx
+++ Projects_react/Colour_changer/src/App.jsx
@@ -1,11 +1,27 @@
-import './App.css'
-
 function App() {
+  const handleClickFunction=(e)=>{
+    //console.log(e.target.value);
+    const body=document.querySelector('body');
+    body.style.background = getRandomColour();
+    e.target.style.backgroundColor= getRandomColour();
+  };
 
+  function getRandomColour(){
+    let letters= '0123456789ABCDEF';
+    let color='#';
+    for(let i=0;i<6;i++){
+      color+=letters[Math.floor(Math.random()*16)];
+    }
+    return color;
+  }
   return (
     <>
+    <div className="container">
+    <h1>Random Background Color Changer</h1>
+    <button className="btn" onClick={handleClickFunction}>Click Me</button>
+    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
